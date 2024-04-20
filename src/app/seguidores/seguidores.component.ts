080ddf6f1a3ff7f712f73fdd14cfed4ae8077de6
Ben-Lajara <rlajaraoton@gmail.com>
2024-04-20 15:17:18 +0200

    Arreglado seguidos y seguidores.

--- src/app/seguidores/seguidores.component.ts
+++ src/app/seguidores/seguidores.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-seguidores',
@@ -10,20 +11,46 @@ import { ActivatedRoute } from '@angular/router';
 export class SeguidoresComponent implements OnInit {
   username='';
   seguidores: any;
-  constructor(private http: HttpClient, private route: ActivatedRoute) { }
+  seguidos: any;
+  currentUser = '';
+  constructor(private http: HttpClient, private route: ActivatedRoute, private authService: AuthService) { }
 
   ngOnInit(): void {
     this.route.params.subscribe((params) => {
       this.username = params['username'];
       console.log(this.username)
-      this.getSeguidores().subscribe((res: any) => {
-        this.seguidores = res;
+      this.currentUser = localStorage.getItem('username') || '';
+      console.log('CurrentUser: '+ this.currentUser)
+      this.getSeguidos().subscribe((res: any) => {
+        this.seguidos = res;
+        console.log(this.seguidos)
+        this.getSeguidores().subscribe((res: any) => {
+          this.seguidores = res.map((usuario: any)=>{
+            usuario.nombreUsuario.seguido = this.seguidos.some((seguido: any) => seguido.usuarioSeguido.nombre === usuario.nombreUsuario.nombre);
+            return usuario;
+          })
+          console.log(this.seguidores)
+        });
       });
+      
       
     });
   }
 
   getSeguidores(){
     return this.http.get(`http://localhost:8080/seguidores/${this.username}`)
+  }
+
+  getSeguidos(){
+    return this.http.get(`http://localhost:8080/seguidosPor/${this.currentUser}`)
+  }
+
+  findUsuarios(nombre: string){
+    this.getSeguidos().subscribe((res: any) => {
+      this.seguidores = res.map((usuario: any)=>{
+        usuario.seguido = this.seguidos.some((seguido: any) => seguido.usuarioSeguido.nombre === usuario.nombre);
+        return usuario;
+      })
+    })
   }
 }
--- src/app/seguidos/seguidos.component.ts
+++ src/app/seguidos/seguidos.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-seguidos',
@@ -10,21 +11,37 @@ import { ActivatedRoute } from '@angular/router';
 export class SeguidosComponent implements OnInit {
   username='';
   seguidos: any;
-  constructor(private http: HttpClient, private route: ActivatedRoute) { }
+  seguidosUsuario: any;
+  currentUser = '';
+  constructor(private http: HttpClient, private route: ActivatedRoute, private authService: AuthService) { }
 
   ngOnInit(): void {
     this.route.params.subscribe((params) => {
       this.username = params['username'];
       console.log(this.username)
-      this.getSeguidos().subscribe((res: any) => {
-        this.seguidos = res;
-      });
+      this.currentUser = localStorage.getItem('username') || '';
+      console.log('CurrentUser: '+ this.currentUser)
+      this.getSeguidosUsuario().subscribe((res: any) => {
+        this.seguidosUsuario = res;
+        console.log(this.seguidosUsuario)
+        this.getSeguidos().subscribe((res: any) => {
+          this.seguidos = res.map((usuario: any)=>{
+            usuario.usuarioSeguido.seguido = this.seguidosUsuario.some((seguido: any) => seguido.nombreUsuario.nombre === this.currentUser);
+            return usuario;
+          })
+          console.log(this.seguidos)
+        });
+      });     
       
     });
   }
 
   getSeguidos(){
     return this.http.get(`http://localhost:8080/seguidosPor/${this.username}`)
+  }
+
+  getSeguidosUsuario(){
+    return this.http.get(`http://localhost:8080/seguidosPor/${this.currentUser}`)
   }
 
 }
